17254fa1c87b72eec0a4153b46390d96ab915b36
Mikeal Rogers <mikeal.rogers@gmail.com>
2009-08-17 01:37:55 -0700

    Adding bulk API support, abstracted HTTP interface, added functionality to RowSet.

--- couchquery/__init__.py
+++ couchquery/__init__.py
@@ -1,27 +1,80 @@
 import os
-import sys
 import urllib
 import copy
+import types
+from urlparse import urlparse
+
 import httplib2
 
 debugging = True
 
 try:
-    import simplejson
+    import simplejson as json
 except:
-    import json as simplejson
+    import json
 
 jheaders = {"content-type":"application/json",
             "accept"      :"application/json"}
 
 design_template = {"_id":"_design/", "language":"javascript"}
 
-class RowsSet(object):
-    def __init__(self, db, rows, parent=None):
+class HttpResponse(object):
+    pass
+
+class Httplib2Response(HttpResponse):
+    def __init__(self, response, content):
+        self.body = content
+        self._response = response
+        self.status = response.status
+        self.headers = response
+
+class HttpClient(object):
+    pass
+    
+class Httplib2Client(HttpClient):
+    def __init__(self, uri, cache=None, http_override=None):
+        self.uri = uri
+        self.parsed = urlparse(uri)
+        if not self.uri.endswith('/'):
+            self.uri += self.uri + '/'
+        
+        if http_override is None:
+            if '@' in self.uri:
+                user, password = self.uri.replace('http://','').split('@')[0].split(':')
+                self.uri = 'http://'+self.uri.split('@')[1]
+                if cache is None:
+                    cache = '.cache'
+                self.http = httplib2.Http(cache)
+                self.http.add_credentials(user, password)
+            else: 
+                self.http = httplib2.Http(cache)
+        else:
+            self.http = http_override
+    
+    def request(self, path, method, headers, body):
+        return self.http.request(self.uri + path, method, headers=headers, body=body)
+    
+    class httplib2MethodWrapper(object):
+        def __init__(self, method):
+            self.method = method
+        def __call__(self, inst, path=None, headers={'content-type':'application/json'}, body=None):
+            resp, content = inst.request(path, self.method, headers=headers, body=body)
+            return Httplib2Response(resp, content)
+    
+    get = httplib2MethodWrapper("GET")
+    put = httplib2MethodWrapper("PUT")
+    post = httplib2MethodWrapper("POST")
+    delete = httplib2MethodWrapper("DELETE")
+        
+
+class RowSet(object):
+    def __init__(self, db, rows, offset=None, total_rows=None, parent=None):
         self.__db = db
         self.__rows = rows
         self.__changes = []
         self.__parent = parent
+        self.__offset = None
+        self.rows = self # Make backwards compatible with older API
     
     def keys(self):
         return [x['key'] for x in self.__rows]
@@ -29,27 +82,36 @@ class RowsSet(object):
         return [x['value'] for x in self.__rows]
     def ids(self):
         return [x['id'] for x in self.__rows]
+        
+    @property
+    def offset(self):
+        if self.__offset is None:
+            if self.__parent.offset is not None:
+                self.__offset = self.__parent.offset + self.__parent.__rows.index(self.__rows[0])
+            else: self.__offset = None
+        return self.__offset
     
     def __iter__(self):
-        # if ( len(self.__rows) is not 0 and type(self.__rows[0]) is dict 
-        #                                    and self.__rows[0].has_key('id') ):
         for x in self.__rows:
             if type(x) is dict and type(x['value']) is dict and x['value'].has_key('_id'):
-                yield CouchDocument(x['value'], db=self.__db)
+                yield Document(x['value'], db=self.__db)
             else:
                 yield x['value']
-        # else:
-        #     for x in self.__rows:
-        #         yield x['value']
+    
+    def __contains__(self, obj):
+        if type(obj) is str:
+            return obj in (x['id'] for x in self.__rows) 
+        else:
+            return obj in (x['value'] for x in self.__rows)
     
     def __getitem__(self, i):
         if type(i) is int:
-            return CouchDocument(self.__rows[i]['value'], db=self.__db)
+            return Document(self.__rows[i]['value'], db=self.__db)
         else:
-            return RowsSet(self.__rows[i], parent=self)
+            return RowSet([r for r in self.__rows if r['key'] == i], parent=self)
         
     def __setattr__(self, name, obj):
-        if name.startswith("__") or "_RowsSet__db":
+        if name.startswith("__") or "_RowSet__db":
             return object.__setattr__(self, name, obj)
         for x in self.__rows:
             x[name] = obj
@@ -60,66 +122,66 @@ class RowsSet(object):
         
     def save(self, ): pass
 
-class ViewResult(dict):
-    def __init__(self, result, db):
-        super(ViewResult, self).__init__(result)
-        self.result = result
-        self.rows = RowsSet(db, result["rows"])
-    def __len__(self):
-        return len(self.result["rows"])
+# class ViewResult(dict):
+#     def __init__(self, result, db):
+#         super(ViewResult, self).__init__(result)
+#         self.result = result
+#         self.rows = RowSet(db, result["rows"])
+#     def __len__(self):
+#         return len(self.result["rows"])
 
 class View(object):
     def __init__(self, design, name, http):
         self.design = design
         self.name = name
-        self.uri = self.design.uri+'_view/'+name+'/'
+        self.path = self.design.path+'_view/'+name+'/'
         self.http = http
     def __call__(self, async=False, twisted=False, callback=None, **kwargs):
         for k, v in kwargs.items():
             if type(v) is bool:
                 kwargs[k] = str(v).lower()
             if k in ['key', 'startkey', 'endkey']:
-                kwargs[k] = simplejson.dumps(v)
+                kwargs[k] = json.dumps(v)
         query_string = urllib.urlencode(kwargs)
         if len(query_string) is not 0:
-            uri = self.uri + '?' + query_string
+            path = self.path + '?' + query_string
         else:
-            uri = self.uri
-        response, content = self.http.request(uri, "GET", headers=jheaders)
+            path = self.path
+        response = self.http.get(path)
         assert response.status == 200
-        response_body = simplejson.loads(content)
-        return ViewResult(response_body, self.design.views.db)
+        result = json.loads(response.body)
+        return RowSet(self.design.views.db, result['rows'], offset=result['offset'], 
+                       total_rows=result['total_rows'])
         
 
 class Design(object):
     def __init__(self, views, name, http):
         self.views = views
         self.name = name
-        self.uri = self.views.uri+self.name+'/'
+        self.path = self.views.path+self.name+'/'
         self.http = http
     def __getattr__(self, name):
         if debugging:    
-            resp, content = self.http.request(self.uri+'_view/'+name+'/', 
-                                                    "HEAD", headers=jheaders)
-        if not debugging or resp.status == 200:
+            response = self.http.head(self.path+'_view/'+name+'/')
+        if not debugging or response.status == 200:
             setattr(self, name, View(self, name, self.http))
             return getattr(self, name)
         else:
-            raise AttributeError("No view named "+name+". "+content)
+            raise AttributeError("No view named "+name+". "+response.body)
 
 class TempViewException(Exception): pass
 
 class Views(object):
     def __init__(self, db, http):
         self.db = db
-        self.uri = self.db.uri+'_design/'
+        self.path = '_design/'
         self.http = http
         
     def temp_view(self, map, reduce=None, **kwargs):
         view = {"map":map}
         if reduce:
             view['reduce'] = reduce
-        body = simplejson.dumps(view)
+        body = json.dumps(view)
         if len(kwargs) is 0:
             uri = self.db.uri+'_temp_view'
         else:
@@ -127,21 +189,22 @@ class Views(object):
                 if type(v) is bool:
                     kwargs[k] = str(v).lower()
                 if k in ['key', 'startkey', 'endkey']:
-                    kwargs[k] = simplejson.dumps(v)
+                    kwargs[k] = json.dumps(v)
             query_string = urllib.urlencode(kwargs)
-            uri = self.db.uri+'_temp_view' + '?' + query_string
+            path = self.path+'_temp_view' + '?' + query_string
 
-        resp, content = self.http.request(uri, "POST", headers=jheaders, body=body)
-        if resp.status == 200:
-            response_body = simplejson.loads(content)
-            return ViewResult(response_body, self.db)
+        response = self.http.post(path, body=body)
+        if response.status == 200:
+            result = json.loads(response.body)
+            return RowSet(self.db, result['rows'], offset=result['offset'], 
+                           total_rows=result['total_rows'])
         else:
-            raise TempViewException('Status: '+str(resp.status)+'\nReason: '+resp.reason+'\nBody: '+content)
+            raise TempViewException('Status: '+str(response.status)+'\nReason: '+response['reason']+'\nBody: '+response.body)
         
     def __getattr__(self, name):
         if debugging:
-            resp, content = self.http.request(self.uri+name+'/', "HEAD", headers=jheaders)
-        if not debugging or resp.status == 200:
+            response = self.http.head(self.path+name+'/')
+        if not debugging or response.status == 200:
             setattr(self, name, Design(self, name, self.http))
             return getattr(self, name)
         else:
@@ -154,71 +217,96 @@ class CouchDBDocumentConflict(Exception): pass
 class CouchDBDocumentDoesNotExist(Exception): pass
 
 class CouchDatabase(object):
-    def __init__(self, uri, http=None, cache=None):
+    def __init__(self, uri, http=None, http_engine=None, cache=None):
         self.uri = uri
-        if not self.uri.endswith('/'):
-            self.uri += '/'
-        
-        if http is None:    
-            if '@' in self.uri:
-                user, password = self.uri.replace('http://','').split('@')[0].split(':')
-                self.uri = 'http://'+self.uri.split('@')[1]
-                if cache is None:
-                    cache = '.cache'
-                http = httplib2.Http(cache)
-                http.add_credentials(user, password)
-            else: 
-                http = httplib2.Http(cache)
-            
-        self.http = http
+        if type(http) is httplib2.Http:
+            self.http = Httplib2Client(uri, http_override=http)
+        elif http_engine is None:
+            self.http = Httplib2Client(uri, cache)
         self.views = Views(self, self.http)
         
     def get(self, _id):
-        resp, content = self.http.request(self.uri+_id, "GET", headers=jheaders)
-        if resp.status == 200:
-            obj = dict([(str(k),v,) for k,v in simplejson.loads(content).items()])
+        """Get a single document by id from the database."""
+        response = self.http.get(_id)
+        if response.status == 200:
+            obj = dict([(str(k),v,) for k,v in json.loads(response.body).items()])
             return CouchDocument(obj, db=self)
         else:
             raise CouchDBDocumentDoesNotExist("No document at id "+_id)
     
-    def create(self, doc):
-        if type(doc) is not dict:
+    def create(self, doc, all_or_nothing=False):
+        """Create a document. Accepts any object that can be converted in to a dict.
+        If multiple documents are passed they are handed off to the bulk document handler.
+        """
+        if type(doc) not in (dict, Document, list, tuple, types.GeneratorType):
             doc = dict(doc)
-        resp, content = self.http.request(self.uri, "POST", headers=jheaders, body=simplejson.dumps(doc))
-        if resp.status == 201:
-            return simplejson.loads(content)
+        
+        # Hand off to bulk handler when passing multiple documents    
+        if type(doc) in (list, tuple, types.GeneratorType):
+            return self.bulk(doc, all_or_nothing=all_or_nothing)
+            
+        response = self.http.post('', body=json.dumps(doc))
+        if response.status == 201:
+            return json.loads(response.body)
         else:
-            raise CouchDBException(content)
+            raise CouchDBException(response.body)
     
-    def update(self, doc):
-        if type(doc) is not dict:
+    def update(self, doc, all_or_nothing=False):
+        """Update a document. Accepts any object that can be converted in to a dict.
+        If multiple documents are passed they are handed off to the bulk document handler.
+        """
+        if type(doc) not in (dict, Document, list, tuple, types.GeneratorType):
             doc = dict(doc)
-        resp, content = self.http.request(self.uri+doc['_id'], "PUT", headers=jheaders, body=simplejson.dumps(doc))
-        if resp.status == 201:
-            return simplejson.loads(content)
-        elif resp.status == 413:
-            raise CouchDBDocumentConflict(content)
+        
+        # Hand off to bulk handler when passing multiple documents    
+        if type(doc) in (list, tuple, types.GeneratorType):
+            return self.bulk(doc, all_or_nothing=all_or_nothing)
+        
+        response = self.http.put(doc['_id'], body=json.dumps(doc))
+        if response.status == 201:
+            return json.loads(response.body)
+        elif response.status == 413:
+            raise CouchDBDocumentConflict(response.body)
         else:
-            raise CouchDBException(content)
+            raise CouchDBException(response.body)
     
-    def delete(self, doc):
-        if type(doc) is not dict:
+    def delete(self, doc, all_or_nothing=False):
+        """Delete a document. Accepts any object that can be converted in to a dict.
+        Document/s must contain _id and _rev properties.
+        If multiple documents are passed they are removed using the bulk document API.
+        """
+        if type(doc) not in (dict, Document, list, tuple, types.GeneratorType):
             doc = dict(doc)
-        resp, content = self.http.request(
-            self.uri+doc['_id']+'?rev='+str(doc['_rev']), 
-            "DELETE", headers=jheaders)
-        if resp.status == 200:
-            return simplejson.loads(content)
+        if type(doc) not in (list, tuple, types.GeneratorType):
+            response = self.http.delete(doc['_id']+'?rev='+str(doc['_rev']))
         else:
-            raise CouchDBException("Delete failed "+content)
+            body = {'rows':[{'_id':doc['_id'], '_rev':doc.get('_rev'), '_deleted':True}],
+                    'all_or_nothing':all_or_nothing}
+            response = self.http.post('_bulk_docs', body=json.dumps(body))
+        
+        if response.status == 200:
+            return json.loads(response.body)
+        else:
+            raise CouchDBException("Delete failed "+response.body)
     
-    def save(self, doc):
-        if type(doc) is not dict:
+    def save(self, doc, all_or_nothing=False):
+        if type(doc) not in (dict, Document, list, tuple, types.GeneratorType):
             doc = dict(doc)
+        
+        # Hand off to bulk handler when passing multiple documents    
+        if type(doc) in (list, tuple, types.GeneratorType):
+            return self.bulk(doc, all_or_nothing=all_or_nothing)    
+            
         if doc.has_key('_id') :
             return self.update(doc)
         else:
             return self.create(doc)
+            
+    def bulk(self, docs, all_or_nothing=False):
+        body = {'docs':docs, 'all_or_nothing':all_or_nothing}
+        response = self.http.post('_bulk_docs', body=json.dumps(body))
+        assert response.status == 200
+        return json.loads(response.body)
 
     def sync_design_doc(self, name, directory):
         document = copy.copy(design_template)
@@ -250,53 +338,37 @@ class CouchDocument(dict):
     def __init__(self, *args, **kwargs):
         if 'db' in kwargs:
             object.__setattr__(self, 'db', kwargs.pop('db'))
-        elif hasattr(self, 'db'): pass
-        else:
-            object.__setattr__(self, 'db', global_db)
         super(CouchDocument, self).__init__(*args, **kwargs)
-
-    # def __getattribute__(self, name):
-    #     if name.startswith('__'):
-    #         return object.__getattribute__(self, name)
-    #     if name in ['save', 'dict', 'db']:
-    #         return object.__getattribute__(self, name)
-    #     obj = self.__getitem__(name)
-    #     if type(obj) is dict:
-    #         obj = CouchDocument(obj)
-    #         self.__setitem__(name, obj)
-    #     return obj    
     
     __getattr__ = dict.__getitem__
-    
     def __setattr__(self, k, v):
-        self[k] = v    
+        self[k] = v
 
+Document = CouchDocument
 
-doc = CouchDocument
-
-from asynchttp import AsyncHTTPConnection
-
-class CouchAsyncConnection(AsyncHTTPConnection):
-    def __init__(self, url, method, obj, callback):
-        self.method = method
-        self.obj = obj
-        self.callback = callback
-        AsyncHTTPConnection.__init__(
-            self, self.host, self.port
-            )
-        self._url = url
-
-    def handle_response(self): 
-        print "results %s %d %s" % (
-            self.response.version,
-            self.response.status,
-            self.response.reason
-            )
-
-    def handle_connect(self):
-        AsyncHTTPConnection.handle_connect(self)
-        self.putrequest("GET", self._url)
-        self.endheaders()
-        self.getresponse()
+# from asynchttp import AsyncHTTPConnection
+# 
+# class CouchAsyncConnection(AsyncHTTPConnection):
+#     def __init__(self, url, method, obj, callback):
+#         self.method = method
+#         self.obj = obj
+#         self.callback = callback
+#         AsyncHTTPConnection.__init__(
+#             self, self.host, self.port
+#             )
+#         self._url = url
+# 
+#     def handle_response(self): 
+#         print "results %s %d %s" % (
+#             self.response.version,
+#             self.response.status,
+#             self.response.reason
+#             )
+# 
+#     def handle_connect(self):
+#         AsyncHTTPConnection.handle_connect(self)
+#         self.putrequest("GET", self._url)
+#         self.endheaders()
+#         self.getresponse()
 
 
