7b0d17ef6d5b0a92b6a4ceeb6c64c2ea8bb82da8
Mikeal Rogers <mikeal.rogers@gmail.com>
2009-10-11 16:43:41 -0700

    Release.

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ desc = """CouchDB Library"""
 summ = """CouchDB Library"""
 
 PACKAGE_NAME = "couchquery"
-PACKAGE_VERSION = "0.8.2"
+PACKAGE_VERSION = "0.8.3"
 
 setup(name=PACKAGE_NAME,
       version=PACKAGE_VERSION,
