c2093c220d0ea5211436351e91afea322cd05b0f
Nicholas Tan Jerome <nicholas.jerome@kit.edu>
2014-08-14 17:16:48 +0200

    added test for compaction method
    Signed-off-by: Nicholas Tan Jerome <nicholas.jerome@kit.edu>

--- tests/test_document_local.py
+++ tests/test_document_local.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 from couchquery import *
-
+import time
 
 BASE_URI = 'http://localhost:5984/'
 URI = BASE_URI + 'couchquery_unittest'
@@ -39,16 +39,34 @@ class DocumentLocal(unittest.TestCase):
         for doc in lectroids:
             self.assertEqual(self.db.create(doc)['ok'], True)
 
-    #def test_bulk_update(self):
-    #    alldocs = self.db.views.all()
-    #    alldocs.species = 'lectroid'
-    #    alldocs.save()
-
-    #def test_views(self):
-    #    rows = self.db.views.banzai.byType()
-        #assert len(rows) is 8
-        #assert type(rows[0]) is Document
-        #assert rows.offset is 0
+    def test_compaction(self):
+        # create doc
+        info = self.db.create({'revision': 1})
+        id_ = info['id']
+        rev_1 = info['rev']
+        # make changes to get 2nd revision
+        doc = self.db.get(id_)
+        doc.revision = 2
+        self.db.update(doc)
+        # make changes to get 3rd revision
+        doc = self.db.get(id_)
+        doc.revision = 3
+        self.db.update(doc)
+        # access both revision, yields true
+        doc = self.db.get(id_, rev_1)
+        self.assertEqual(doc.revision, 1)
+        # run compaction
+        self.assertEqual(self.db.compaction(), True)
+        # ensure compaction process are done
+        time.sleep(5)
+        # checking availability of previous revisions
+        is_compacted = True
+        revs = self.db.get_revs(id_)
+        for i, rev in enumerate(revs):
+            if i > 0:
+                is_compacted = False
+            print i, rev
+        self.assertEqual(is_compacted, True)
 
     def tearDown(self):
         deletedb(self.db)
